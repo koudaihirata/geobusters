ee2c0fada4dc4a46ea300a96c05e216b7a3fcec1
koudaihirata <2230051@ecc.ac.jp>
2026-01-16 16:54:06 +0900

    Add:カード画像のフォルダ整理と画像の追加

--- frontend/src/utils/cards.ts
+++ frontend/src/utils/cards.ts
@@ -11,11 +11,11 @@ export type CardMeta = {
 
 export const CARD_LIBRARY: Record<number, CardMeta> = {
     /* 攻撃カード */
-    101: { id: 101, label: '木の剣', detail: '1ダメージを与える', category: 'attack', requiresTarget: true, allowSelfTarget: true, img: 'woodenSword' },
+    101: { id: 101, label: '木の剣', detail: '1ダメージを与える', category: 'attack', requiresTarget: true, allowSelfTarget: true, img: 'cards/woodenSword' },
     102: { id: 102, label: '真剣', detail: '3ダメージを与える', category: 'attack', requiresTarget: true, allowSelfTarget: true, img: '' },
     /* 防御カード */
-    201: { id: 201, label: 'ボロボロの盾', detail: '1ダメージ防ぐ', category: 'defense', img: '' },
+    201: { id: 201, label: 'ボロボロの盾', detail: '1ダメージ防ぐ', category: 'defense', img: 'cards/woodenShield' },
     202: { id: 202, label: '量産型シールド', detail: '3ダメージ防ぐ', category: 'defense', img: '' },
     /* 回復カード */
-    301: { id: 301, label: '癒しの薬草', detail: 'HPが1回復（HPが満タンの時最大体力が増える）', category: 'heal', requiresTarget: true, allowSelfTarget: true, img: '' },
+    301: { id: 301, label: '癒しの薬草', detail: 'HPが1回復（HPが満タンの時最大体力が増える）', category: 'heal', requiresTarget: true, allowSelfTarget: true, img: 'cards/medicinalHerbs' },
 }
