96b5a24a5bbe90dd0e89af11e9f8cdbeb644615d
koudaihirata <2230051@ecc.ac.jp>
2026-01-19 14:07:28 +0900

    update: resolveCardImgSrc関数のデフォルト画像を'noImage.svg'に変更し、noImage.svgファイルを追加

--- frontend/src/utils/resolveCardImg.ts
+++ frontend/src/utils/resolveCardImg.ts
@@ -5,5 +5,5 @@ export const resolveCardImgSrc = (card: CardMeta) => {
     if (card.img && card.img.startsWith('data:')) return card.img
     if (card.img && card.img.length > 100) return `data:image/png;base64,${card.img}`
     if (card.img) return `${card.img}.svg`
-    return 'Group.svg'
+    return 'noImage.svg'
 }
